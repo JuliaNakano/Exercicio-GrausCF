0b68cd5be2e6840c697099f8edc4ee9f37e0817d
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-19 19:11:00 -0300

    Solução do exercício GrausCF

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            double celsius, fahrenheit;
+
+
+            Console.Write("\nDigite o valor em graus celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            fahrenheit = celsius* 1.8 + 32;
+            
+            Console.WriteLine($"\n{celsius}° é equivalente à {fahrenheit}°F\n");
+            
+        }
+
+    }
+}
